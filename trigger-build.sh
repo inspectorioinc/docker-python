2566e4cc9be669bad09ca5b5034f045ccf55ea43
Thuan Duong <thuandt26@gmail.com>
2019-08-19 12:58:04 +0700

    Add --silent option for curl

--- trigger-build.sh
+++ trigger-build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 for t in 2 2.7 3 3.6 3.7; do
-    curl --request POST \
+    curl --silent \
+         --request POST \
          --header "Content-Type:application/json" \
          --data "{\"dockerfile\":\"/$t\",\"source_name\":\"master\",\"source_type\":\"Branch\",\"docker_tag\":\"$t\"}" \
          --url "${DOCKER_HUB_TRIGGER_URL}"
