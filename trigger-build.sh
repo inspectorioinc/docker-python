ecd7592a501578bf2672c408e94c239b0606439a
Thuan Duong <thuandt26@gmail.com>
2019-08-19 12:51:20 +0700

    Add GitLab configuration for trigger Docker Hub builds

---
+++ trigger-build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+for t in 2 2.7 3 3.6 3.7; do
+    curl --request POST \
+         --header "Content-Type:application/json" \
+         --data "{\"dockerfile\":\"/$t\",\"source_name\":\"master\",\"source_type\":\"Branch\",\"docker_tag\":\"$t\"}" \
+         --url "${DOCKER_HUB_TRIGGER_URL}"
+done
