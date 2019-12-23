d8abbb9b17b2d205992c8ade5b2b87cb4b561d71
Thuan Duong <thuandt26@gmail.com>
2019-12-24 00:45:14 +0700

    Tweak trigger build script

--- trigger-build.sh
+++ trigger-build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-for t in 2 2.7 3 3.6 3.7; do
-    curl --silent \
-         --request POST \
-         --header "Content-Type:application/json" \
-         --data "{\"dockerfile\":\"/$t\",\"source_name\":\"master\",\"source_type\":\"Branch\",\"docker_tag\":\"$t\"}" \
-         --url "${DOCKER_HUB_TRIGGER_URL}"
+set -eou pipefail
+
+for t in 2 2.7 3 3.6 3.7 3.8; do
+  echo "Send build request for Python ${t} to Docker Hub..."
+  curl --silent \
+    --request POST \
+    --header "Content-Type:application/json" \
+    --data "{\"dockerfile\":\"/$t\",\"source_name\":\"master\",\"source_type\":\"Branch\",\"docker_tag\":\"$t\"}" \
+    --url "${DOCKER_HUB_TRIGGER_URL}" | jq .state
+  echo
 done
